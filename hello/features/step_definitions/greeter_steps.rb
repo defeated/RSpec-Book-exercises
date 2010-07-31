6f1011aae52deb6190d86f6e94cbf59e2316bf22
Eddie Cianci <defeated2k4@gmail.com>
2010-07-31 11:11:44 -0400

    made steps fail

--- hello/features/step_definitions/greeter_steps.rb
+++ hello/features/step_definitions/greeter_steps.rb
@@ -1,11 +1,11 @@
 Given /^a greeter$/ do
-  pending # express the regexp above with the code you wish you had
+  @greeter = CucumberGreeter.new
 end
 
 When /^I send it the greet message$/ do
-  pending # express the regexp above with the code you wish you had
+  @message = @greeter.greet
 end
 
 Then /^I should see "([^"]*)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+  @message.should == greeting
 end
